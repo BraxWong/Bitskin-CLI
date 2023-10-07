ebfce2b06ead5ca728d6eecf0b4844aa0e7e3dd9
Brax Wong <ghostenjob@gmail.com>
2023-10-07 17:45:51 +0100

    Adding comments

--- src/CLI/header/ArgumentListener.h
+++ src/CLI/header/ArgumentListener.h
@@ -6,6 +6,17 @@
 using json = nlohmann::json;
 class ArgumentListener {
   public:
+
+/*╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+  ┃                                                                              ┃
+  ┃      Function: displayArgumentInfoOnly() Description: This method will       ┃
+  ┃    display the specific information being passed in. For example, if -arg    ┃
+  ┃   wbalance is present when called the -session command, only the wbalance    ┃
+  ┃                section of the json object will be displayed.                 ┃
+  ┃                                                                              ┃
+  ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+*/
+
     bool displayArgumentInfoOnly(json j, std::string input);
 };
 #endif
