b05248b51a252ded29d6df5fcbea8a12a42b0ac7
Brax Wong <ghostenjob@gmail.com>
2023-10-08 13:22:36 +0100

    Testing out the Catch2 framework. It works!!!!

--- tests/tests.cxx
+++ tests/tests.cxx
@@ -1,2 +1,24 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark.hpp>
+#include "../src/CLI/header/UserCredentials.h"
+bool returnTrue()
+{
+  return true;
+}
+
+bool test2()
+{
+  UserCredentials* uc = new UserCredentials();
+  if(uc != nullptr)
+  {
+    return true;
+  }
+  return false;
+}
+TEST_CASE("returnTrue()","[returnTrue]")
+{
+  REQUIRE(returnTrue() == true);
+  REQUIRE(test2() == true);
+}
+
+
