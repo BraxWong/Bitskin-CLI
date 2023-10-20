9a85477b25900be2a68d4c3c6185b603b1c247d2
Brax Wong <ghostenjob@gmail.com>
2023-10-20 16:04:09 +0100

    Remove unnecessary header files from main.cxx

--- main.cxx
+++ main.cxx
@@ -1,21 +1,6 @@
-#include "src/Account/header/Affiliate.h"
-#include "src/Config/header/CurrencyRates.h"
-#include "src/Config/header/FeePlans.h"
-#include "src/Config/header/PlatformStatus.h"
-#include "src/Market/header/Pricing.h"
-#include "src/Market/header/MarketItems.h"
-#include "src/CLI/header/MainMenu.h"
-#include "src/Account/header/Profile.h"
-#include "src/CLI/header/Listener.h"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <nlohmann/json.hpp>
-#include <string>
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
 #include "memplumber.h"
-
+#include "src/CLI/header/Listener.h"
+#include "src/CLI/header/MainMenu.h"
 
 
 /*       ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
