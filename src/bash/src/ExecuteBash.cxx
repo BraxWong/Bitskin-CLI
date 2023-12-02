f825e47ee4650eb5258427e9b60657d304f63917
Brax Wong <ghostenjob@gmail.com>
2023-12-02 11:07:41 +0000

    Reading and storing output when curling

--- src/bash/src/ExecuteBash.cxx
+++ src/bash/src/ExecuteBash.cxx
@@ -2,6 +2,8 @@
 #include <initializer_list>
 #include <iostream>
 #include <stdio.h>
+#include <cstdio>
+#include <sstream>
 
 bool ExecuteBash::executeBashScript(std::string url, std::string APIKey, std::vector<std::string> keys, std::vector<std::string> values, std::vector<std::string> dataTypes)
 {
@@ -11,17 +13,43 @@ bool ExecuteBash::executeBashScript(std::string url, std::string APIKey, std::ve
   {
     command << arg << " ";
   }
-  if(system(command.str().c_str()) == 0)
+  FILE* pipe = popen(command.str().c_str(), "r");
+  if (!pipe)
   {
-    system("echo \"\"");
-    return true;
+    throw std::runtime_error("popen() failed!");
   }
+
+  std::ostringstream output;
+  char buffer[128];
+  while (fgets(buffer, sizeof(buffer), pipe) != nullptr)
+  {
+    output << buffer;
+  }
+
+  // Close the pipe
+  pclose(pipe);
+
+  //Return the captured output as a string
+  std::string str = output.str();
+  std::string res;
+  for(int i = 0; i < str.length(); ++i)
+  {
+    if(str[i] == '{')
+    {
+      for(int j = i; j < str.length(); ++j)
+      {
+        res += str[j];
+      }
+      break;
+    }
+  }
+  std::cout << res << "\n";
   return false;
 }
 
 std::vector<std::string> ExecuteBash::createBashCMD(std::string url, std::string APIKey, std::vector<std::string> keys, std::vector<std::string> values, std::vector<std::string> dataTypes)
 {
-  std::string curlURL = "curl " + url;
+  std::string curlURL = "curl -s " + url;
   std::string post = "-X POST";
   std::string contentType = "-H 'content-type: application/json'";
   std::string apiKey = "-H 'x-apikey: " + APIKey + "'";
