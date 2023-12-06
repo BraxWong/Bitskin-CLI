7eaf6c00b85dabc5383d71a65066446455ad4365
Brax Wong <ghostenjob@gmail.com>
2023-12-06 21:09:25 +0000

    Created sendEmail function in EmailSender to send emails when users have subscribed to an event

---
+++ src/EmailSender/src/EmailSender.cxx
@@ -0,0 +1,27 @@
+#include "../header/EmailSender.h"
+
+void EmailSender::sendEmail(std::string emailContent, std::string subject)
+{
+  Poco::Net::MailMessage mm; 
+  mm.addRecipient(Poco::Net::MailRecipient(Poco::Net::MailRecipient::PRIMARY_RECIPIENT, "ghostenjob@gmail.com", "Bob"));
+  mm.setSubject(subject);
+  mm.setSender(this->emailAddr);
+  mm.setContent(emailContent);
+  
+  Poco::Net::SMTPClientSession* clientSession = new Poco::Net::SMTPClientSession("smtp.gmail.com", 465);
+  clientSession->login();
+  try
+  {
+    clientSession->sendMessage(mm);
+  }
+  catch(const Poco::Net::SMTPException& e)
+  {
+    std::cout << e.message() << "\n";
+  }
+  catch(const Poco::Net::NetException& netException)
+  {
+    std::cout << netException.message() << "\n";
+  }
+  clientSession->close();
+  delete clientSession;
+}
