d5223eacf77f35599b59789b92381eed33bee5ba
Brax Wong <83001805+BraxWong@users.noreply.github.com>
2024-08-23 21:48:01 +0800

    test: added tests for writeAPIKeyToFile() function.

--- tests/UserCredentialsTests.cxx
+++ tests/UserCredentialsTests.cxx
@@ -19,6 +19,21 @@ bool testAuthTokenValidity(UserCredentials *uc, std::string AuthToken) {
   return false;
 }
 
+bool testWriteAPIKeyToFile(UserCredentials *uc, std::string APIKey) {
+  bool testPassed = true;
+  if(uc->writeAPIKeyToFile(APIKey) == false) {
+    testPassed = false; 
+  }
+  else {
+    std::string fileAPIKey;
+    fileAPIKey = uc->readAPIKeyFromFile();
+    if(APIKey != fileAPIKey) {
+      testPassed = false;
+    }
+  }
+  return testPassed;
+}
+
 TEST_CASE("testAuthTokenValidity()", "[testAuthTokenValidity]") {
   std::cout << "Running testAuthTokenValidity() test.\n";
   UserCredentials *uc = new UserCredentials();
@@ -43,4 +58,12 @@ TEST_CASE("testAPIKeyLengthValidity()", "[testAPIKeyLengthValidity]")
   REQUIRE(testAPIKeyLengthValidity(uc, "1234582347189372189073291873289173289173089217308912730812703811") == true);
   REQUIRE(testAPIKeyLengthValidity(uc, "123458234718937218907329187328917328917308921730891273081270381") == false);
   delete uc;
+}
+
+TEST_CASE("testWriteAPIKeyToFile()", "[testWriteAPIKeyToFile]")
+{
+  std::cout << "Running testWriteAPIKeyToFile() test.\n";
+  UserCredentials* uc = new UserCredentials();
+  REQUIRE(testWriteAPIKeyToFile(uc, "328907589013728904378910-42132143612648675786457546546455214214") == false);
+  REQUIRE(testWriteAPIKeyToFile(uc, "328907589013728904378910-421321436126486757864575465464552142141") == true);
 }
